46bed8d5173876b3601759ffea8ae3ec0b6fb11d
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-26 18:14:46 -0300

    calculos node

---
+++ modules/calc.js
@@ -0,0 +1,10 @@
+var SomaFunc = require("./somar");
+var SubFunc = require("./sub");
+var MultiFunc = require("./multi");
+var DivFunc = require("./div");
+
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(10,5));
+console.log(MultiFunc(10,2));
+console.log(DivFunc(10,2));
