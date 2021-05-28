07440bc2e64c94fcc03774ba765d05bb62ce0d02
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-27 22:47:35 -0300

    handlebars

--- projeto-express/index.js
+++ projeto-express/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize');
+
 
 //Config
     //Template Engine
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars') 
+// Conexão com o banco de dados MySql
 
+const sequelize = new Sequelize('estudonode', 'root', 'root', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+//Rotas
+
+    app.get('/cad', function(req, res){
+        res.render('formulario.')
+    })
 
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081");
