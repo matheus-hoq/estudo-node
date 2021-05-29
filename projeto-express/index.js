a80add9050c13d0deccbb6d5a324a2348ad3ce98
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-29 00:37:28 -0300

    posts + estrutura do banco de dados

--- projeto-express/index.js
+++ projeto-express/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize')
+const Post = require('./models/Post')
 
 
 //Config
@@ -12,21 +12,27 @@ const Sequelize = require('sequelize')
 //Body Parser
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(bodyParser.json())
-// Conexão com o banco de dados MySql
 
-const sequelize = new Sequelize('estudonode', 'root', 'root', {
-    host: "localhost",
-    dialect: 'mysql'
-})
 
 //Rotas
+
+    app.get('/', function(req, res){
+        res.render('home')
+    })
 
     app.get('/cad', function(req, res){
         res.render('formulario')
     })
 
     app.post('/add', function(req,res){
-         res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo)
+         Post.create({
+             titulo: req.body.titulo,
+             conteudo: req.body.conteudo
+         }).then(function(){
+             res.redirect('/')
+         }).catch(function(erro){
+             res.send("Houve um erro: " + erro)
+         })
     })
 
 app.listen(8081, function(){
