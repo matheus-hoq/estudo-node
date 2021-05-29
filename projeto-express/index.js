8e8aba257fc8a1d7a026cb0bfcfa71e3dfafe9a9
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-29 00:13:46 -0300

    bodyParser

--- projeto-express/index.js
+++ projeto-express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 
 
@@ -8,6 +9,9 @@ const Sequelize = require('sequelize')
     //Template Engine
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars') 
+//Body Parser
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
 // Conexão com o banco de dados MySql
 
 const sequelize = new Sequelize('estudonode', 'root', 'root', {
@@ -22,7 +26,7 @@ const sequelize = new Sequelize('estudonode', 'root', 'root', {
     })
 
     app.post('/add', function(req,res){
-        res.send('FORMULARIO RECEBIDO"')
+         res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo)
     })
 
 app.listen(8081, function(){
