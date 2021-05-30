974487b2a7ccea90ca456e51bf1c3e13879d570c
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-30 00:10:04 -0300

    Curso de Node.js - Trabalhando com o Mongoose #30

--- estudo-mongo/mongoTeste.js
+++ estudo-mongo/mongoTeste.js
@@ -10,3 +10,59 @@ mongoose.connect("mongodb://localhost/testemongo", {
 }).catch((err) => {
     console.log("Houve um erro ao se conectar com o MongoDB")
 })
+
+// Model - Usuários
+//Definindo o model
+const UsuarioSchema = mongoose.Schema({
+
+nome:{
+    type: String,
+    require: true
+},
+sobrenome: {
+    type: String,
+    require: true
+},
+email: {
+    type: String,
+    require: true
+},
+idade: {
+    type: Number,
+    require: true
+},
+país: {
+    type: String
+}
+})
+
+// Collection
+
+mongoose.model('usuarios', UsuarioSchema )
+
+const Math = mongoose.model('usuarios')
+
+new Math({
+    nome: "John",
+    sobrenome: "Doe",
+    idade: 34,
+    email: "email123@email.com",
+    país: "Brasil"
+}).save().then(() => {
+    console.log("Usuário cadastrado com sucesso!")
+}).catch((err) => {
+    console.log("Houve um erro ao registrar o usuário" +err)
+})
+
+// new UsuarioSchema({
+//     nome:  "Math",
+//     sobrenome: "hoq",
+//     email: "email@email.com",
+//     idade: 20,
+//     país: "Brasil"
+// }).save().then(() => {
+//     console.log("Usuário criado com sucesso!")
+// }).catch((err) => {
+//     console.log("Houve um erro ao registrar o usuário: "+err)
+// })
+
