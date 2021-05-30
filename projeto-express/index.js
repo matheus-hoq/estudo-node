ab7498dcd73beb3f60a877a43bb94c5bd45b44c5
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-29 23:00:36 -0300

    Curso de Node.js - Deletando posts #26 - final

--- projeto-express/index.js
+++ projeto-express/index.js
@@ -39,6 +39,14 @@ const { Console } = require("console");
          })
     })
 
+    app.get('/deletar/:id', function(req,res){
+        Post.destroy({where: {'id': req.params.id}}).then(function(){
+            res.send("Postagem deletada com sucesso!")
+        }).catch(function(erro){
+            res.send("Esta postagem não existe! ")
+        })
+    })
+
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081");
 });
