fdc04002440c24615874b41ccb1df890991e447a
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-30 22:35:50 -0300

    Curso de Node.js - Listando categorias #41 + erro handlebars resolvido

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -14,7 +14,13 @@ router.get('/posts', (req,res) => {
 })
 
 router.get('/categorias',(req,res) => {
-    res.render("admin/categorias")
+    Categoria.find().sort({date:'desc'}).then((categorias) =>{
+        res.render('admin/categorias', {categorias: categorias.map(categoria => categoria.toJSON())})   
+        //    res.render("admin/categorias", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias")
+        res.redirect("/admin")
+    })
 })
 
 router.get('/categorias/add',(req,res) => {
