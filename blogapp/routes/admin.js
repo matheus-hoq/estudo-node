80801816e41d61ce1b80adc3e098d911d12b671f
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-31 00:09:20 -0300

    Curso de Node.js - Editando categoria #42 - erro ao salvar alterações

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -59,9 +59,36 @@ router.post('/categorias/nova',(req,res) => {
             res.redirect("/admin")
         })
     }
+})
 
+router.get("/categorias/edit/:id", (req,res) =>{
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria) => {
+        res.render("admin/editcategorias", {categoria: categoria})
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe")
+        res.redirect("/admin/categorias")
+    })
+})
 
+router.post("/categorias/edit", (req,res) => {
 
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            res.flash("success_msg", "Categoria editada com sucesso")
+            res.redirect("/admin/categorias")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria")
+            res.redirect("admin/categorias")
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar a categoria")
+        res.redirect("/admin/categorias")
+    })
 })
 
 
