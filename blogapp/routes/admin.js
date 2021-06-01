911b039fc6d8b68a24ee3c98f6ddcb46d70ab7bb
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-05-31 21:40:00 -0300

    Curso de Node.js - Deletando categorias #43 - bug encontrado

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -91,6 +91,16 @@ router.post("/categorias/edit", (req,res) => {
     })
 })
 
+router.post("/categorias/deletar", (req,res) => {
+    Categoria.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso!")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao deletar a categoria")
+        res.redirect("/admin/categorias")
+    })
+})
+
 
 router.get('/teste',(req,res) => {
     res.send("Isso é um teste")
