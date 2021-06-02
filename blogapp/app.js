206f3e6307b7f0523840ada076ee0f3fb2045349
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-06-02 14:46:48 -0300

    Curso de Node.js - Finalizando autenticação #59

--- blogapp/app.js
+++ blogapp/app.js
@@ -28,11 +28,12 @@ require("./config/auth")(passport)
     app.use(passport.initialize())
     app.use(passport.session())
     app.use(flash())
-    
+
     //middleware
         app.use((req, res, next) => {
             res.locals.success_msg = req.flash("success_msg")
             res.locals.error_msg = req.flash("error_msg")
+            res.locals.error = req.flash("error")
             next()
         })
     //body parser
--- blogapp/config/auth.js
+++ blogapp/config/auth.js
@@ -10,7 +10,7 @@ const Usuario = mongoose.model("usuarios")
 
 module.exports = function(passport){
 
-    passport.use(new localStrategy({usernameField: 'email'}, (email, senha, done) => {
+    passport.use(new localStrategy({usernameField: 'email', passwordField: "senha"}, (email, senha, done) => {
 
         Usuario.findOne({email: email}).then((usuario) => {
             if(!usuario){
@@ -19,7 +19,7 @@ module.exports = function(passport){
 
             bcrypt.compare(senha, usuario.senha, (erro, batem) => {
                 if(batem){
-                    return done(null, user)
+                    return done(null, usuario)
                 }else{
                     return done(null, false, {message: "Senha incorreta"})
                 }
@@ -36,8 +36,8 @@ module.exports = function(passport){
     })
 
     passport.deserializeUser((id, done) => {
-        User.findById(id, (err, usuario) => {
-            done(err, user)
+        Usuario.findById(id, (err, usuario) => {
+            done(err, usuario)
         })
     })
 
