1ca8e07d44228e0289af2810b74354083d87ee15
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-06-02 11:02:09 -0300

    Curso de Node.js - Fazendo correção #55

--- blogapp/models/Usuario.js
+++ blogapp/models/Usuario.js
@@ -10,6 +10,12 @@ const Usuario = new Schema({
         type: String,
         required: true
     },
+
+    eAdmin: {
+        type: Number,
+        default: 0
+    },
+
     senha: {
         type: String,
         required: true
