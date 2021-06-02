9f4ff1d8178f0821d638edf9a9a53508e7c1a986
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-06-01 21:27:44 -0300

    algumas alterações no código + busca erro ao criar categoria

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -13,9 +13,9 @@ router.get('/posts', (req,res) => {
     res.send("Página de posts")
 })
 
-router.get('/categorias',(req,res) => {
+router.get("/categorias",(req,res) => {
     Categoria.find().sort({date:'desc'}).then((categorias) =>{
-        res.render('admin/categorias', {categorias: categorias.map(categoria => categoria.toJSON())})   
+        res.render("admin/categorias", {categorias: categorias.map(categoria => categoria.toJSON())})   
         //    res.render("admin/categorias", {categorias: categorias})
     }).catch((err) => {
         req.flash("error_msg", "Houve um erro ao listar as categorias")
@@ -23,11 +23,11 @@ router.get('/categorias',(req,res) => {
     })
 })
 
-router.get('/categorias/add',(req,res) => {
+router.get("/categorias/add",(req,res) => {
     res.render("admin/addcategorias")
 })
 
-router.post('/categorias/nova',(req,res) => {
+router.post("/categorias/nova",(req,res) => {
     
     var erros = []
 
