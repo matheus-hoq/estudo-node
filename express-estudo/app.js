7a7905804eef3fe209de31449574bccbc5f6a077
Ricardo Monteiro <ricardohenrique071@gmail.com>
2021-10-04 16:56:10 -0300

    new server variable

--- express-estudo/app.js
+++ express-estudo/app.js
@@ -17,6 +17,8 @@ app.get('/ola/:cargo/:nome/:cor', function(req,res){
     res.send("<h1>Ola "+req.params.nome+"</h1>"+"<h2> Seu cargo e: "+req.params.cargo+"</h2>"+"<h3> Sua cor favorita e: "+req.params.cor+"</h3>");
 })
 
-app.listen(8081, function(){
-    console.log("Servidor rodando na url http://localhost:8081")
+var server_code = 3000;
+
+app.listen(server_code, function(){
+    console.log("Servidor rodando na url ${server_code}")
 });
